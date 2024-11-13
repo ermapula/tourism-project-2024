1355a404486741b89ea3369cac3d6577a049882e
Ermek <tolymbekov_ermek@mail.ru>
2024-11-13 18:11:03 +0600

    add: footer component, MaterialUI package

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,48 @@
+import { Box, Modal, Typography } from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  boxShadow: 24,
+  p: 4,
+  bgcolor: 'white',
+}
+
 export default function Footer(params) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
-      Footer
+      <footer>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            About us
+          </Typography>
+        </Box>
+      </Modal>
+        <div className="footer-container">
+          <div className="footer-copyright">
+            &#169; 2024
+          </div>
+          <div>
+            <Link onClick={handleOpen} style={{textDecoration: 'none'}}>About Us</Link>
+          </div>
+          <div></div>
+        </div>
+      </footer>
     </>
   ) 
 }
