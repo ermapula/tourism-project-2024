419eceeee76cfecf86d93fdf2f6b83175d6fb6cd
Ermek <tolymbekov_ermek@mail.ru>
2025-01-26 00:15:50 +0600

    troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const backendUrl = process.env.VITE_BACKEND_URL;
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export default defineConfig({
   plugins: [react()],
