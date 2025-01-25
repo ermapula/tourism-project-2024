2c59e95033060f737ad62887dba721d22610196d
Ermek <tolymbekov_ermek@mail.ru>
2025-01-25 16:28:42 +0600

    add: more crud apis (Location, Tour, Ticket, Category)

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,8 +1,14 @@
 import axiosPrivate from "./initPrivate";
 
 
+// Users
 export async function getUsers(link) {
   const response = link ? await axiosPrivate.get(link) : await axiosPrivate.get(`/api/users/`);
+  return response.data;
+}
+
+export async function getUser(id) {
+  const response = await axiosPrivate.get(`/api/users/${id}`);
   return response.data;
 }
 
@@ -21,8 +27,20 @@ export async function deleteUser(id) {
   return response.data;
 }
 
+// Categories
 export async function getCategories(link) {
   const response = link ? await axiosPrivate.get(link) : await axiosPrivate.get(`/api/categories/`);
+  return response.data;
+}
+
+export async function getCategoriesAll() {
+  const r1 = await axiosPrivate.get(`/api/categories/`);
+  const response = await axiosPrivate.get(`/api/categories/?size=${r1.data.count}`)
+  return response.data;
+}
+
+export async function getCategory(id) {
+  const response = await axiosPrivate.get(`/api/categories/${id}/`);
   return response.data;
 }
 
@@ -38,5 +56,91 @@ export async function updateCategory(id, data) {
 
 export async function deleteCategory(id) {
   const response = await axiosPrivate.delete(`/api/categories/${id}/`);
+  return response.data;
+}
+
+// Locations
+export async function getLocations(link) {
+  const response = link ? await axiosPrivate.get(link) : await axiosPrivate.get(`/api/locations/`);
+  return response.data;
+}
+
+export async function getLocation(id) {
+  const response = await axiosPrivate.get(`/api/locations/${id}/`);
+  return response.data;
+}
+
+export async function createLocation(data) {
+  const response = await axiosPrivate.post(`/api/locations/`, data, {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
+  });
+  return response.data;
+}
+
+export async function updateLocation(id, data) {
+  const response = await axiosPrivate.patch(`/api/locations/${id}/`, data, {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
+  });
+  return response.data;
+}
+
+export async function deleteLocation(id) {
+  const response = await axiosPrivate.delete(`/api/locations/${id}/`);
+  return response.data;
+}
+
+// Tours
+export async function getTours(link) {
+  const response = link ? await axiosPrivate.get(link) : await axiosPrivate.get(`/api/tours/`);
+  return response.data;
+}
+
+export async function getTour(id) {
+  const response = await axiosPrivate.get(`/api/tours/${id}/`);
+  return response.data;
+}
+
+export async function addTour(data) {
+  const response = await axiosPrivate.post("/api/tours/", data);
+  return response.data;
+}
+
+export async function updateTour(id, data) {
+  const response = await axiosPrivate.patch(`/api/tours/${id}/`, data);
+  return response.data;
+}
+
+export async function deleteTour(id) {
+  const response = await axiosPrivate.delete(`/api/tours/${id}/`);
+  return response.data;
+}
+
+// Ticket
+export async function getTickets(link) {
+  const response = link ? await axiosPrivate.get(link) : await axiosPrivate.get(`/api/tickets/`);
+  return response.data;
+}
+
+export async function getTicket(id) {
+  const response = await axiosPrivate.get(`/api/tickets/${id}/`);
+  return response.data;
+}
+
+export async function addTicket(data) {
+  const response = await axiosPrivate.post("/api/tickets/", data);
+  return response.data;
+}
+
+export async function updateTicket(id, data) {
+  const response = await axiosPrivate.patch(`/api/tickets/${id}/`, data);
+  return response.data;
+}
+
+export async function deleteTicket(id) {
+  const response = await axiosPrivate.delete(`/api/tickets/${id}/`);
   return response.data;
 }
