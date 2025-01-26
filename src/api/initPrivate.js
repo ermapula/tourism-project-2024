1a72cdae6224f405e32638e9ff25d396715d23fe
Ermek <tolymbekov_ermek@mail.ru>
2025-01-27 01:18:29 +0600

    fix: deploy issues

--- src/api/initPrivate.js
+++ src/api/initPrivate.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import axiosPublic, { baseURL } from "./initPublic";
 
 const axiosPrivate = axios.create({
+  baseURL: baseURL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_')
-  return {
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_BACKEND_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-        '/media': {
-          target: env.VITE_BACKEND_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-      }
-    },
-    plugins: [react()],
-  }
+export default defineConfig({
+  plugins: [react()],
 })
